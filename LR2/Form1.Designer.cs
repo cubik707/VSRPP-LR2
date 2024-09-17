794c000a1f202fbf58de051bf7b11ffabe43cef6
Vladislava <stdemidovez@gmail.com>
2024-09-17 17:37:12 +0300

    Add project files.

---
+++ LR2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LR2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.delete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Redo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(25, 366);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 0;
+            this.delete.Text = "Удалить";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(811, 459);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Redo
+            // 
+            this.Redo.Location = new System.Drawing.Point(25, 395);
+            this.Redo.Name = "Redo";
+            this.Redo.Size = new System.Drawing.Size(114, 23);
+            this.Redo.TabIndex = 2;
+            this.Redo.Text = "Восстановить";
+            this.Redo.UseVisualStyleBackColor = true;
+            this.Redo.Click += new System.EventHandler(this.Redo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Redo);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Redo;
+    }
+}
---
+++ LR2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR2
+{
+    public partial class Form1 : Form
+    {
+        //для выбора цвета
+        private Color currentColor = Color.Black;
+        //хранение объектов
+        private List<Rectangle> drawnObjects = new List<Rectangle>();
+        //хранение удаленных объектов
+        private Stack<List<Rectangle>> undoStack = new Stack<List<Rectangle>>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Redo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
